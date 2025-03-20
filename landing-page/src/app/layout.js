2752ad228568840f55d43621fa0e7c1decf6f2f5
MistaCareca <brennno0cartaxo@gmail.com>
2025-03-20 14:04:40 -0300

    hot fix: ajuste para build

--- landing-page/src/app/layout.js
+++ landing-page/src/app/layout.js
@@ -1,14 +1,14 @@
-import '..app/globals.css'
+import '../app/globals.css';
 
 export const metadata = {
   title: 'Minha Landing Page',
   description: 'Uma landing page incrível com Next.js e Tailwind',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body className="antialiased">{children}</body>
     </html>
-  )
+  );
 }
